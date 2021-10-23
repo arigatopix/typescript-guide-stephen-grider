2f56c18d9162ea97d7efca36d825e6390d7de2d4
arigatopix-notebook <teeruch.k@gmail.com>
2021-10-23 21:06:55 +0700

    22. Fixing the 'Any' Type

--- section-03-type-annotations/annotations/variables.ts
+++ section-03-type-annotations/annotations/variables.ts
@@ -28,3 +28,11 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use 'Annotations'
+// 1) Function that returns the 'Any' type
+const json = '{ "x": 10, "y": 20 }';
+
+const coordinates: { x: number; y: number } = JSON.parse(json);
+
+console.log(coordinates);
