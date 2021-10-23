ff068cddd1a44fff73c780c9cb43af276fe89482
arigatopix-notebook <teeruch.k@gmail.com>
2021-10-23 17:48:51 +0700

    9. Catching Errors with Typescript

--- section-01-started/01-fetchjson/index.ts
+++ section-01-started/01-fetchjson/index.ts
@@ -1,17 +1,20 @@
 import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/2';
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 axios.get(url).then(response => {
-  const todo = response.data;
+  const todo = response.data as Todo; // กำหนดให้ todo รับจาก interface
 
-  const ID = todo.id;
-  const title = todo.Title;
-  const finished = todo.finished;
+  const { id, title, completed } = todo;
 
   console.log(`
-    The Todo with ID: ${ID}
+    The Todo with ID: ${id}
     Has a title of: ${title}
-    Is it finished? ${finished}
+    Is it finished? ${completed}
   `);
 });
