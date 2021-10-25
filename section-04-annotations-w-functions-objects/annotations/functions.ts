374b7ddd1b8c5dec8439c841d23f6ece945b336a
arigatopix-notebook <teeruch.k@gmail.com>
2021-10-25 17:48:00 +0700

    30. Annotations Around Objects

--- section-04-annotations-w-functions-objects/annotations/functions.ts
+++ section-04-annotations-w-functions-objects/annotations/functions.ts
@@ -54,3 +54,22 @@ const logWeather = ({ date, weather }: { date: Date; weather: String }) => {
 };
 
 logWeather(todaysWeather);
+
+// Type Annotations for Object (nested)
+const profile = {
+  name: 'alex',
+  age: 29,
+  coords: {
+    lat: 0,
+    lng: 15,
+  },
+  setAge(age: number): void {
+    this.age = age;
+  },
+};
+
+const { age, name }: { age: number; name: string } = profile;
+
+const {
+  coords: { lat, lng },
+}: { coords: { lat: number; lng: number } } = profile;
