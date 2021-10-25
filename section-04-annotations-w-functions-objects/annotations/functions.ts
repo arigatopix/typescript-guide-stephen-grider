7bdfff6fbb8561397449ee4c0a9ff7e8e18eb50f
arigatopix-notebook <teeruch.k@gmail.com>
2021-10-25 16:55:29 +0700

    26. Inference Around Functions

--- section-04-annotations-w-functions-objects/annotations/functions.ts
+++ section-04-annotations-w-functions-objects/annotations/functions.ts
@@ -1,3 +1,8 @@
-const add = (a: number, b: number): number => {
+const add = (a: number, b: number) => {
   return a + b;
+};
+
+// Type Annotations for return Value
+const subtract = (a: number, b: number): number => {
+  return a - b;
 };
