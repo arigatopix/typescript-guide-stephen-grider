c7e9c9f314329e181fd2164fa2f032106904234f
arigatopix-notebook <teeruch.k@gmail.com>
2021-10-25 17:15:29 +0700

    28. Void and Never

--- section-04-annotations-w-functions-objects/annotations/functions.ts
+++ section-04-annotations-w-functions-objects/annotations/functions.ts
@@ -15,3 +15,29 @@ function devide(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
   return a * b;
 };
+
+// Function return null or return undefined
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+// Function throw error (not return anything but execute function)
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+// - return something
+const throwError2 = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+
+  return 'return someting';
+};
+
+// - return null
+const throwError3 = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+};
