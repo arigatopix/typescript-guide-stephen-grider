22ef4884beae701077d972a7882ef106d25eeddf
arigatopix-notebook <teeruch.k@gmail.com>
2021-10-25 18:41:47 +0700

    31. Arrays in Typescript

---
+++ section-05-typed-arrays/arrays.ts
@@ -0,0 +1,9 @@
+const carMakers = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+// assign type annotations
+const carMakers2: string[] = ['honda', 'issuzu'];
+
+// 2 dimension array
+// const carByMake = [['f150'], ['corolla']];
+const carByMake: string[][] = [];
