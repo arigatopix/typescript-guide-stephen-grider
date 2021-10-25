ff1bf07484a5054564129bbf88490622652cd2f1
arigatopix-notebook <teeruch.k@gmail.com>
2021-10-25 18:49:58 +0700

    32. Why Typed Arrays?

--- section-05-typed-arrays/arrays.ts
+++ section-05-typed-arrays/arrays.ts
@@ -7,3 +7,16 @@ const carMakers2: string[] = ['honda', 'issuzu'];
 // 2 dimension array
 // const carByMake = [['f150'], ['corolla']];
 const carByMake: string[][] = [];
+
+// Why use Type for arrays
+//Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(120);
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
