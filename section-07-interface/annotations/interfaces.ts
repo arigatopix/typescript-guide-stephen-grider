25db843af649285e88a5f53e42578b21eaee9938
arigatopix-notebook <teeruch.k@gmail.com>
2021-10-26 12:18:00 +0700

    40. Fixing Long Annotations with Interfaces

--- section-07-interface/annotations/interfaces.ts
+++ section-07-interface/annotations/interfaces.ts
@@ -1,15 +1,19 @@
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
 const oldCivic = {
   name: 'civic',
   year: 2000,
   broken: true,
 };
 
-const prinVehicle = (vehicle: {
-  name: string;
-  year: number;
-  broken: boolean;
-}): void => {
+const prinVehicle = (vehicle: Vehicle): void => {
   console.log(`Name: ${vehicle.name}`);
   console.log(`Year: ${vehicle.year}`);
   console.log(`Is broken?: ${vehicle.broken}`);
 };
+
+prinVehicle(oldCivic);
