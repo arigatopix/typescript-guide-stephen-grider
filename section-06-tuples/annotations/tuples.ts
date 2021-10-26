1ca0fed32a218dae50618e827125f36c90c3675b
arigatopix-notebook <teeruch.k@gmail.com>
2021-10-26 10:25:53 +0700

    36. Tuples in Action

---
+++ section-06-tuples/annotations/tuples.ts
@@ -0,0 +1,21 @@
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+};
+
+// ----  Arrays -----
+// const pepsi = ['brown', true, 40];
+// // change position
+// pepsi[0] = true;
+// pepsi[2] = 'brown';
+
+// ----  TUPLES -----
+// const pepsi: [string, boolean, number] = ['brown', true, 40];
+
+// Type alias
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['clear', true, 40];
+const tea: Drink = ['brown', false, 0];
