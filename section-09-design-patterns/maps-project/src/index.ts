706a603f62f88fffe811579840a20bceb29bc973
arigatopix-notebook <teeruch.k@gmail.com>
2021-11-06 15:40:15 +0700

    65. Why Use Private Modifiers? Here's Why

--- section-09-design-patterns/maps-project/src/index.ts
+++ section-09-design-patterns/maps-project/src/index.ts
@@ -1,10 +1,9 @@
 // import { User } from './User';
 // import { Company } from './Company';
 
-new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+import { customMap } from './customMap';
+
+new customMap('map');
+
+//create other div id
+new customMap('other');
