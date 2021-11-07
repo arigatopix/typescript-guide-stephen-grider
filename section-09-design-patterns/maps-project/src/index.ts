67eee87187af224363d7787f25290f815c42bdae
arigatopix-notebook <teeruch.k@gmail.com>
2021-11-07 12:47:27 +0700

    73. Optional Implements Clauses

--- section-09-design-patterns/maps-project/src/index.ts
+++ section-09-design-patterns/maps-project/src/index.ts
@@ -1,7 +1,7 @@
 import { User } from './User';
 import { Company } from './Company';
 
-import { CustomMap } from './customMap';
+import { CustomMap } from './CustomMap';
 
 const user = new User();
 const company = new Company();
