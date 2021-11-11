2a74740dc5a92db5b2cc35031121e31f7ddaddd7
arigatopix-notebook <teeruch.k@gmail.com>
2021-11-11 11:17:23 +0700

    79. Sorter Scaffolding

--- section-10-more-design-patterns/sort/src/index.ts
+++ section-10-more-design-patterns/sort/src/index.ts
@@ -1,5 +1,16 @@
-const logSomthing = () => {
-  console.log('Somthing');
-};
+class Sorter {
+  constructor(public collection: number[]) {
+    // argument
+  }
 
-logSomthing();
+  sort(): void {
+    // method
+  }
+}
+
+// initial instance
+const sorter = new Sorter([3, -1, -5, 0, -12, 4]);
+
+sorter.sort();
+
+console.log(sorter.collection);
