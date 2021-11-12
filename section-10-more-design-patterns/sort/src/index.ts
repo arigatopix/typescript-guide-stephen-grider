d1ea734ccba487505c30822555ba23223d7f8de5
arigatopix-notebook <teeruch.k@gmail.com>
2021-11-12 12:40:50 +0700

    83. Type Guards

--- section-10-more-design-patterns/sort/src/index.ts
+++ section-10-more-design-patterns/sort/src/index.ts
@@ -6,20 +6,27 @@ class Sorter {
   sort(): void {
     const { length } = this.collection;
 
+    // All of this only works if collection is number[]
     // If collection is an array of numbers
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
+        // TypeGuards
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
         }
       }
     }
-  }
 
-  // If collection is a string, do this logic instead:
-  // ~~~ logic to compare and swap characters in a string
+    // Only going to work if collection s string
+    // If collection is a string, do this logic instead:
+    // ~~~ logic to compare and swap characters in a string
+    if (typeof this.collection === 'string') {
+    }
+  }
 }
 
 // initial instance
