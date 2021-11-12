3fa6e47097e6a499dfee1de48a35395344ce288b
arigatopix-notebook <teeruch.k@gmail.com>
2021-11-12 15:45:55 +0700

    89. Sorting Arbitrary Collections

--- section-10-more-design-patterns/sort/src/index.ts
+++ section-10-more-design-patterns/sort/src/index.ts
@@ -1,10 +1,20 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-const numbersCollection = new NumbersCollection([3, -1, -5, 0, -12, 4]);
+// * Number Collection
+// const numbersCollection = new NumbersCollection([3, -1, -5, 0, -12, 4]);
 
-const sorter = new Sorter(numbersCollection);
+// const sorter = new Sorter(numbersCollection);
 
+// sorter.sort();
+
+// console.log(numbersCollection.data);
+
+// * Character Collection
+const charactersCollection = new CharactersCollection('XaaYb');
+const sorter = new Sorter(charactersCollection);
+
+// sorting
 sorter.sort();
-
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
