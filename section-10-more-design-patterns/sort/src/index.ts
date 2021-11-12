4750d7779336a17f8ee0e36cbfeb561068525ea0
arigatopix-notebook <teeruch.k@gmail.com>
2021-11-12 14:24:14 +0700

    86. Separating Swapping and Comparison

--- section-10-more-design-patterns/sort/src/index.ts
+++ section-10-more-design-patterns/sort/src/index.ts
@@ -1,37 +1,10 @@
-class Sorter {
-  constructor(public collection: number[] | string) {
-    // argument
-  }
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
 
-  sort(): void {
-    const { length } = this.collection;
+const numbersCollection = new NumbersCollection([3, -1, -5, 0, -12, 4]);
 
-    // All of this only works if collection is number[]
-    // If collection is an array of numbers
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        // TypeGuards
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-      }
-    }
-
-    // Only going to work if collection s string
-    // If collection is a string, do this logic instead:
-    // ~~~ logic to compare and swap characters in a string
-    if (typeof this.collection === 'string') {
-    }
-  }
-}
-
-// initial instance
-const sorter = new Sorter([3, -1, -5, 0, -12, 4]);
+const sorter = new Sorter(numbersCollection);
 
 sorter.sort();
 
-console.log(sorter.collection);
+console.log(numbersCollection.data);
