13ea826a06e50c16e6cdaa27aab43773fa0e4b97
arigatopix-notebook <teeruch.k@gmail.com>
2021-11-14 13:31:08 +0700

    104. Running an Analysis

--- section-11-reusable-code/stats/src/index.ts
+++ section-11-reusable-code/stats/src/index.ts
@@ -9,4 +9,20 @@ const matches = fs
     return row.split(',');
   });
 
-console.log(matches);
+// Analyze 'Man United' wins
+let manUnitedWins = 0;
+
+// loop array
+for (let match of matches) {
+  // check match แต่ละ column
+
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    // Check Home team and Home win
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    // Check Away team and Away win
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
