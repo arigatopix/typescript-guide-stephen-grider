8915c74528992a303c6b8d72845c191b400bdbf4
arigatopix <teeruch.k@gmail.com>
2022-06-11 18:39:56 +0700

    217. Using an Express Router

--- section-14-express/server/src/index.ts
+++ section-14-express/server/src/index.ts
@@ -1,13 +1,9 @@
 import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-    <div>
-      <h1>Hi There!</h1>
-    </div>
-  `);
-});
+app.use(router);
 
 const PORT = 5000 || process.env.PORT;
 
---
+++ section-14-express/server/src/routes/loginRoutes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+  res.send(`
+  <div>
+    <h1>Hi There!</h1>
+  </div>
+`);
+});
+
+export { router };
