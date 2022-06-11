4de94b2ffd958103d292c618cd919965acef2bc6
arigatopix <teeruch.k@gmail.com>
2022-06-11 14:42:03 +0700

    135. create static method MatchReader csv

--- section-11-reusable-code/stats/src/index.ts
+++ section-11-reusable-code/stats/src/index.ts
@@ -1,22 +1,8 @@
-import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { Summary } from './Summary';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { HtmlReport } from './reportTargets/HtmlReport';
 
-// Create an object that satisfies the 'DataReader' Interface
-const csvFileReader = new CsvFileReader('football.csv');
-
-// Create an instance of MatchReader and pass in something sattisfying the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
-
-// Load file
-matchReader.load();
-
-// OUTPUT
-// const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
-
+const matchReader = MatchReader.fromCsv('football.csv');
 const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
+matchReader.load();
 summary.buildAndPrintReport(matchReader.matches);
