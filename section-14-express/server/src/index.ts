7721ba2228485a5017ebc5bb525f8595f5239a71
arigatopix <teeruch.k@gmail.com>
2022-06-11 18:31:31 +0700

    216. Basic Routes with Express

--- section-14-express/server/src/index.ts
+++ section-14-express/server/src/index.ts
@@ -1,1 +1,16 @@
-console.log('hi bas')
+import express, { Request, Response } from 'express';
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1>Hi There!</h1>
+    </div>
+  `);
+});
+
+const PORT = 5000 || process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
