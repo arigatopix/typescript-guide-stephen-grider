6e6abad04b89501eaa68fd9f1c49413074c42766
arigatopix <teeruch.k@gmail.com>
2022-06-12 11:47:35 +0700

    224. Logging Out

--- section-14-express/server/src/routes/loginRoutes.ts
+++ section-14-express/server/src/routes/loginRoutes.ts
@@ -53,4 +53,9 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
   }
 });
 
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect('/');
+});
+
 export { router };
