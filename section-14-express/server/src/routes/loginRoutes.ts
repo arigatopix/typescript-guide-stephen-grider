020dc6dbb5334d5fad480bd0d6d1a2e6526f4ff1
arigatopix <teeruch.k@gmail.com>
2022-06-12 11:45:32 +0700

    223. Checking Login Status

--- section-14-express/server/src/routes/loginRoutes.ts
+++ section-14-express/server/src/routes/loginRoutes.ts
@@ -6,6 +6,24 @@ interface RequestWithBody extends Request {
 
 const router = Router();
 
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+    <div>
+      <div>You are logged in</div>
+      <a href="/logout">Logout</a>
+    </div>
+    `);
+  } else {
+    res.send(`
+    <div>
+      <div>You are not logged in</div>
+      <a href="/login">Login</a>
+    </div>
+    `);
+  }
+});
+
 router.get('/login', (req: Request, res: Response) => {
   res.send(`
   <form method="POST">
