ffc1faed00078fb3daa0ff546c92e76f40c029ca
arigatopix <teeruch.k@gmail.com>
2022-06-12 11:39:55 +0700

    222. Wiring Up Sessions

--- section-14-express/server/src/index.ts
+++ section-14-express/server/src/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 import { router } from './routes/loginRoutes';
 
 const app = express();
 
 // MIDDLEWARE
+app.use(cookieSession({ keys: ['ourapp'] }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(router);
