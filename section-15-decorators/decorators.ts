99ef7e9dd43fbcc43f54f6731db7f0fdea09679d
arigatopix <teeruch.k@gmail.com>
2022-06-12 14:59:31 +0700

    235. Decorators Around Properties

--- section-15-decorators/decorators.ts
+++ section-15-decorators/decorators.ts
@@ -1,4 +1,5 @@
 class Boat {
+  @testDecorator
   color: string = 'red';
 
   get formattedColor(): string {
@@ -10,6 +11,10 @@ class Boat {
     throw new Error();
     console.log('swish');
   }
+}
+
+function testDecorator(target: any, key: string) {
+  console.log(target.color);
 }
 
 // Decorators Factories
@@ -26,5 +31,3 @@ function logError(errorMessage: string) {
     };
   };
 }
-
-new Boat().pilot();
